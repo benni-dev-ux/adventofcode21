3117b6c9b1944333440a17e1c0cac634ab09e997
Benedikt <benediktsteinmueller@googlemail.com>
2021-11-30 00:12:21 +0100

    some formatting

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Advent of Code 2021
+
+Solutions to the advent of code 2021 in Python
+https://adventofcode.com/2021
--- day_template/dayX.py
+++ day_template/dayX.py
@@ -1,10 +1,10 @@
 from input_helper import *
 
 
-def do_stuff():
+def solve():
     input_list = read_input('input.txt')
     print(input_list)
 
 
 if __name__ == "__main__":
-    do_stuff()
+    solve()
