21f631fb0c21137af29e13e48759a65af091fcaa
Benedikt <benediktsteinmueller@googlemail.com>
2021-12-01 15:58:00 +0100

    solved day1 part2

---
+++ day_1/day1_part2.py
@@ -0,0 +1,27 @@
+from input_helper import *
+
+
+def solve():
+    input_list = read_input('input.txt')
+
+    increases = 0
+    previous_sum = 0
+
+    i = 1
+    while i < (len(input_list) - 2):
+        first = input_list[i]
+        second = input_list[i + 1]
+        third = input_list[i + 2]
+
+        sum = first + second + third
+
+        if sum > previous_sum:
+            increases += 1
+        previous_sum = sum
+        i += 1
+
+    print("Depth Increased " + str(increases) + " times")
+
+
+if __name__ == "__main__":
+    solve()
