c7ab8ccbb9af079bcd7750b58108adfa8d3e4c69
Benedikt <benediktsteinmueller@googlemail.com>
2021-12-03 13:51:54 +0100

    clunky solution to part1

--- day_3/day3_part1.py
+++ day_3/day3_part1.py
@@ -1,10 +1,62 @@
-from input_helper import *
 from timeit import default_timer as timer
+
+from input_helper import *
 
 
 def solve():
     input_list = read_input('input.txt')
-    print(input_list)
+    input_seperated = []
+    # split into digits
+    for i in input_list:
+        x = [int(a) for a in str(i)]
+        input_seperated.append(x)
+
+    gamma_rate = binary_most_common(input_seperated)
+    gamma_str = ''.join(map(str, gamma_rate))
+    epsilon_rate = invert_binary(gamma_rate)
+    epsilon_str = ''.join(map(str, epsilon_rate))
+    print(gamma_str)
+    print(epsilon_str)
+
+    # Cast do decimal and multiply
+    solution = (int(gamma_str, 2)) * (int(epsilon_str, 2))
+    print(solution)
+
+
+def invert_binary(input):
+    output = ([])
+    for d in input:
+        if d == 0:
+            output.append(1)
+        else:
+            output.append(0)
+    return output
+
+
+def binary_most_common(input):
+    line_length = 12
+    input_length = len(input)
+    digit = 0
+    output = ([])
+
+    # Iterate over maximum number of digits in a line
+    while digit < line_length:
+        current_digit = ([])
+
+        # Add each element to current digit array
+        for element in input:
+            current_digit.append(element[digit])
+
+        ones = current_digit.count(1)
+        # Check if 1 occurs more than half the time
+        if ones > (input_length / 2):
+            output.append(1)
+        else:
+            output.append(0)
+
+        digit += 1
+
+    return output
 
 
 if __name__ == "__main__":
--- day_3/input_helper.py
+++ day_3/input_helper.py
@@ -1,4 +1,4 @@
 def read_input(filename):
     '''reading text file line by line'''
     with open(filename) as f:
-        return [int(line) for line in f.read().splitlines()]
+        return [str(line) for line in f.read().splitlines()]
