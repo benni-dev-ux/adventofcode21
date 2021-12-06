440c1e2b1ff795e40eb6184989fe9e8856babc48
Benedikt <benediktsteinmueller@googlemail.com>
2021-12-06 17:43:02 +0100

    added solution to part2

--- day_6/day6_part2.py
+++ day_6/day6_part2.py
@@ -1,10 +1,33 @@
-from input_helper import *
 from timeit import default_timer as timer
+
+import numpy as np
+
+from input_helper import *
+
+
+def initialize_fish_counter(input_list):
+    counter = np.zeros(9)
+    for f in input_list:
+        counter[f] += 1
+    return counter
 
 
 def solve():
     input_list = read_input('input.txt')
-    print(input_list)
+
+    fish_counter = initialize_fish_counter(input_list)
+    days = 256
+
+    for _ in range(days):
+        fish_at_timer_zero = fish_counter[0]
+        for d in range((len(fish_counter))):
+            if d < 8:
+                fish_counter[d] = fish_counter[d + 1]
+
+        fish_counter[6] += fish_at_timer_zero
+        fish_counter[8] = fish_at_timer_zero
+
+    print(f"Fish amount after {days} days: {np.sum(fish_counter)}")
 
 
 if __name__ == "__main__":
