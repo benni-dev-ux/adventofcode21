5a4eb8b1705bd77024b7474542ed914ad48a9a03
Benedikt <benediktsteinmueller@googlemail.com>
2021-12-06 11:46:47 +0100

    doing some cleanup

--- day_5/day5_part1.py
+++ day_5/day5_part1.py
@@ -6,7 +6,7 @@ from input_helper import *
 
 
 def resolve_vent_lines(arr, grid):
-    # create empry arr with the shape of input arr
+    # create empty array with the shape of input arr
     for line in arr:
         ho_diff = line[0] - line[2]
         ve_diff = line[1] - line[3]
@@ -15,10 +15,9 @@ def resolve_vent_lines(arr, grid):
             start = (line[0])
             end = (line[2])
 
-            if (start < end):
-                temp = start
+            # swap values if difference is negative
+            if start < end:
                 start = end
-                end = temp
                 ho_diff *= -1
             for i in range(ho_diff + 1):
                 grid[line[1], start - i] += 1
@@ -27,10 +26,9 @@ def resolve_vent_lines(arr, grid):
             start = (line[1])
             end = (line[3])
 
-            if (start < end):
-                temp = start
+            # swap values if difference is negative
+            if start < end:
                 start = end
-                end = temp
                 ve_diff *= -1
             for i in range(ve_diff + 1):
                 grid[start - i, line[0]] += 1
@@ -54,32 +52,26 @@ def solve():
     col_2 = np.max(arr[:, 2])
     col_3 = np.max(arr[:, 3])
 
-    max_x =col_0
-    if col_2>max_x:
+    max_x = col_0
+    if col_2 > max_x:
         max_x = col_2
 
     max_y = col_1
     if col_3 > max_y:
         max_y = col_3
 
-    print(max_x)
-    print(max_y)
-
-
-
-    grid = np.zeros((max_x+1, max_y+1))
-
+    grid = np.zeros((max_x + 1, max_y + 1))
 
     # only keep vertical and horizontal lines
     # probably not the most efficient way to do this in numpy
     horizontal_lines = arr[arr[:, 0] == arr[:, 2]]
     vertical_lines = arr[arr[:, 1] == arr[:, 3]]
     arr = np.append(horizontal_lines, vertical_lines, axis=0)
+
     # draw lines
     grid = resolve_vent_lines(arr, grid)
-    print(grid)
     crossings = count_crossings(grid)
-    print(crossings)
+    print(f"Overlapping lines: {crossings}")
 
 
 if __name__ == "__main__":
