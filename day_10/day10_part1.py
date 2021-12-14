f36c249dd4eb425030dd508a4f62e55883263b93
Benedikt <benediktsteinmueller@googlemail.com>
2021-12-14 19:22:44 +0100

    progress and cleanup

--- day_10/day10_part1.py
+++ day_10/day10_part1.py
@@ -1,54 +1,66 @@
 from timeit import default_timer as timer
 
 from input_helper import *
-import numpy as np
 
 
 def solve():
-    input_list = read_input('test_input.txt')
+    input_list = read_input('input.txt')
 
     score = 0
 
     for input in input_list:
-        queue = []
-        char_counter = [0, 0, 0, 0]
-        for digit in input:
-            print(digit)
-
+        # print(len(input_list))
+        stack = []
+        for idx, digit in enumerate(input):
+            exit = False
             match digit:
-                case"(":
-                    char_counter[0] += 1
-                case"[":
-                    char_counter[1] += 1
-                case"{":
-                    char_counter[2] += 1
-                case"<":
-                    char_counter[3] += 1
-                case")":
-                    char_counter[0] -= 1
-                    if char_counter[0] < 0:
-                        print("found ) ")
-                        score += 3
-                        # break
-                case"]":
-                    char_counter[1] -= 1
-                    if char_counter[1] <= 0:
-                        print("found ] ")
-                        score += 57
-                       # break
-                case"}":
-                    char_counter[2] -= 1
-                    if char_counter[2] <= 0:
-                        print("found} ")
-                        score += 1197
-                       # break
-                case">":
-                    char_counter[3] -= 1
-                    if char_counter[3] <= 0:
-                        print("found> ")
-                        score += 25137
-                       # break
+                case "(":
+                    stack.append(digit)
 
+                case "[":
+                    stack.append(digit)
+
+                case "{":
+                    stack.append(digit)
+
+                case "<":
+                    stack.append(digit)
+
+                case ")":
+                    obj = stack[-1]
+                    if obj != '(':
+                        print("expected ( at " + str(idx) + " in line " + str(input))
+                        score += 3
+                        exit = True
+                    del stack[-1]
+
+                case "]":
+                    obj = stack[-1]
+                    if obj != '[':
+                        print("expected [ at " + str(idx) + " in line " + str(input))
+                        score += 57
+                        exit = True
+                    del stack[-1]
+
+                case "}":
+                    obj = stack[-1]
+                    if obj != '{':
+                        print("expected { at " + str(idx) + " in line " + str(input))
+                        score += 1197
+                        exit = True
+                    del stack[-1]
+
+                case ">":
+                    obj = stack[-1]
+                    if obj != '<':
+                        print("expected < at " + str(idx) + " in line " + str(input))
+                        score += 25137
+                        exit = True
+                    del stack[-1]
+
+            if exit:
+                input_list.remove(input)
+                break
     print(score)
 
 
