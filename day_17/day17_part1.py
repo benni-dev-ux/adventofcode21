6bc3c446b42583a08db133d8d6f7931c0588be2e
Benedikt <benediktsteinmueller@googlemail.com>
2021-12-19 00:16:56 +0100

    updating day 17 part1

--- day_17/day17_part1.py
+++ day_17/day17_part1.py
@@ -1,6 +1,36 @@
 from timeit import default_timer as timer
 
+from tqdm import tqdm
+
 from input_helper import *
+
+
+def shoot_probe(x_vel, y_vel, x_target, y_target):
+    max_y = -99999999
+    x = 0
+    y = 0
+    pastTarget = False
+
+    while not pastTarget:
+
+        x += x_vel
+        y += y_vel
+        if y > max_y:
+            max_y = y
+
+        if x_target[0] <= x <= x_target[1] and y_target[0] <= y <= y_target[1]:
+            return int(max_y)
+        elif y_target[0] > y:
+            pastTarget = True
+
+        if x_vel > 0:
+            x_vel += 1
+        elif x_vel < 0:
+            x_vel -= 1
+
+        y_vel -= 1
+
+    return -99999999
 
 
 def solve():
@@ -20,6 +50,18 @@ def solve():
 
     print(x_target)
     print(y_target)
+
+    max_height = -99999999
+    brute_force_target = 500
+
+    for y in tqdm(range(y_target[0], brute_force_target)):
+        for x in range(1, x_target[1]):
+            height = shoot_probe(x, y, x_target, y_target)
+
+            if height > max_height:
+                max_height = height
+
+    print(max_height)
 
 
 if __name__ == "__main__":
